ed56e3e6d466dcdc9664909d6c1353bf8007bec8
d1abl023 <suruceaanu1998@gmail.com>
2020-02-15 16:32:51 +0200

    removed js

--- src/main/resources/static/js/utils/ui_manager.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var UIManager = /** @class */ (function () {
-    function UIManager() {
-        window.onload = function () { return console.error(">>> url: " + window.location); };
-    }
-    return UIManager;
-}());
-exports.UIManager = UIManager;
